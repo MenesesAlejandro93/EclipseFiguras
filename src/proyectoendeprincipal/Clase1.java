65a40fed8abbb4a1245b1432c720f0020c2790d1
orion <orion@DESKTOP-HUI8TTJ>
2024-01-25 19:52:40 +0100

    commit4

--- src/proyectoendeprincipal/Clase1.java
+++ src/proyectoendeprincipal/Clase1.java
@@ -14,7 +14,7 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code applicationv logic here
     }
     
 }
